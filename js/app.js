a5b93eceb37d7cc2e700e08ca6b7930c5e69b856
Ryan Osten <ostenrj@gmail.com>
2017-04-29 12:20:54 -0400

    making get request

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 'use strict'
 
-const T = require('./config.js');
+const config = require ('./config.js');
+const Twit = require('twit');
 const express = require('express');
 const app = express();
+const T = new Twit(config);
+
+
+app.use('/static', express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/templates');
 
 app.get('/', (req, res) => {
-  res.render(index.pug);
+  const tweets = T.get('statuses/user_timeline', {screen_name: 'r_osto', count: 5}, function(err, data, response){
+    return data;
+  })
+
+  console.log(tweets);
+  //res.render(index.pug);
+  res.send('cool!');
 });
 
 app.listen(3000, () => {
