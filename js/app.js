48759609fb93a1f74eaec047f20823e05112291f
Ryan Osten <ostenrj@gmail.com>
2017-04-29 09:46:52 -0400

    init server

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 'use strict'
 
 const T = require('./config.js');
+const express = require('express');
+const app = express();
 
-console.log(T);
+app.get('/', (req, res) => {
+  res.render(index.pug);
+});
+
+app.listen(3000, () => {
+  console.log("The frontend server is running on port 3000!");
+});
