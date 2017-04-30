9b4616adb606b64ea1b7bce3f35276cebbbc25fe
Ryan Osten <ostenrj@gmail.com>
2017-04-30 14:35:55 -0400

    fixes

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'M2boSXnp1LlOxDJt8EdGaXIIz',
+  consumer_secret:      'gjv4JCDtetSXVAcaEcC5oC51P0GH16XAyRw2D82hAQ0TS1JKpM',
+  access_token:         '233404204-KgucBjHUKNcfMxf0Nb9WkojysCfX6AWKfh1e7he1',
+  access_token_secret:  'DyKxZaYHn5WMnHyrr7My1NuDtQg7l43GxGi8UqTe8bQzG',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
