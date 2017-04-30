28a92f6170018566a80e24e71e781470639aad88
Ryan Osten <ostenrj@gmail.com>
2017-04-30 12:12:21 -0400

    added promise to API call

--- js/app.js
+++ js/app.js
@@ -6,17 +6,29 @@ const express = require('express');
 const app = express();
 const T = new Twit(config);
 
-
+/*
 app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/templates');
+*/
 
 app.get('/', (req, res) => {
-  const tweets = T.get('statuses/user_timeline', {screen_name: 'r_osto', count: 5}, function(err, data, response){
-    return data;
-  })
 
-  console.log(tweets);
+  const tweetsPromise = new Promise((resolve, reject) => {
+      T.get('statuses/user_timeline', {screen_name: 'r_osto', count: 5}, function(err, data, response){
+      resolve(data);
+    });
+  });
+
+  const handlePromiseResults = function(value){
+    console.log(value);
+  }
+
+  const handlePromiseError = function(e){
+    console.log(e);
+  }
+
+  tweetsPromise.then(handlePromiseResults).catch(handlePromiseError);
   //res.render(index.pug);
   res.send('cool!');
 });
