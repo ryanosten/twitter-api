8f79755170e91ae11f3a9eee60fc080f0bf92be9
Ryan Osten <ostenrj@gmail.com>
2017-05-13 11:59:58 -0400

    refactored routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@
 
 const express = require('express');
 const app = express();
-const home = require('./routes/home');
-const postTweet = require('./routes/post-tweet')
+const routes = require('./routes/index');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
@@ -14,9 +13,7 @@ app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/templates');
 
-app.use('/', home);
-
-app.post('/', postTweet);
+app.use('/', routes);
 
 app.listen(3000, () => {
   console.log("The frontend server is running on port 3000!");
