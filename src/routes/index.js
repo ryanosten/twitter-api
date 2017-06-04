270514df95b4a943c8b04ac365fa35f5e6da42c1
Ryan Osten <ostenrj@gmail.com>
2017-06-04 08:33:44 -0400

    cleaned up commented code

--- src/routes/index.js
+++ src/routes/index.js
@@ -84,64 +84,6 @@ router.post('/', (req, res) => {
   //make post request to twitter with Twit
   T.post('/statuses/update', {status: tweet});
 
-  /*
-  res.redirect('/');
-
-  //construct promise for get tweets
-  const tweetsPromise = T.get('statuses/user_timeline', {screen_name: 'r_osto', count: 5});
-
-  //Promise for get friends list
-  const friendsPromise = T.get('friends/list', {screen_name: 'r_osto', count: 5});
-
-  //promise for get messages
-  const messagesPromise = T.get('direct_messages', {count: 5});
-
-  //make promise array to pass to Promise.all() function
-  const promiseArray = [tweetsPromise, friendsPromise, messagesPromise];
-
-  //handle all promise data results
-  Promise.all(promiseArray).then((promisesResolvedArray) => {
-
-    //store all promise data
-    let tweetData = promisesResolvedArray[0].data;
-    let friendsData = promisesResolvedArray[1].data;
-    let messages = promisesResolvedArray[2].data;
-
-    //get user information and store
-    let users = tweetData[0].user;
-
-    //tweets object to pass to template
-    let tweets = {};
-
-    //loop through tweetData and add objects to tweets variable
-    for (let item of tweetData){
-      let tweet_key = `tweet${tweetData.indexOf(item)}`;
-      tweets[tweet_key] = {
-                          timestamp: item.created_at.substring(3, 16),
-                          text: item.text,
-                          name: item.user.name,
-                          username: item.user.screen_name,
-                          user_image: item.user.profile_image_url,
-                          retweet: item.retweet_count,
-                          favorited: item.favorite_count
-                          }
-    }
-
-    //store friendsData.users
-    let friends = friendsData.users;
-
-    //render template
-    res.render('index', { users: users,
-                          tweets: tweets,
-                          friends: friends,
-                          messages: messages
-                        });
-  //catch errors
-}).catch( (err) => {
-    let error = new Error(err);
-    res.send(error);
-  });
-*/
 });
 
 
